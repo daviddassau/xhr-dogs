e19200b7e7cb99e23f21e03fea4e7070ebd3bafe
David Dassau <todieahero@hotmail.com>
2017-08-28 18:47:51 -0500

    reading from dogs.json

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log("I'm in main.js");
+
+function runThisAfterDogsLoads(){
+	var data = JSON.parse(this.responseText).dogs;
+	console.log("dogs", data);
+	// domString(data.dogs);
+}
+
+function shitsBroke(){
+	console.log("WHAT DID YOU DO?");
+}
+
+var myDawgs = new XMLHttpRequest();
+myDawgs.addEventListener("load", runThisAfterDogsLoads);
+myDawgs.addEventListener("error", shitsBroke);
+myDawgs.open("GET", "dogs.json");
+myDawgs.send();
